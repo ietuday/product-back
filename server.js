f02714945975033986625f238bb10e0d4cfac805
udayaditya.singh <udayaditya.singh@outlook.com>
2017-12-10 01:36:56 +0530

    second Commit - GetAll API

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = express();
-const db             = require('./config/db');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+var cors = require('cors')
 
-const port = 3000;
+const db = require('./config/db');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const app = express();
+
+const port = 8000;
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extened: true }));
+
 
 MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
-  });               
+
+    if (err) return console.log(err)
+        //require('./app/routes')(app,{});
+        //check below line changed
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log("We are live on" + port);
+    });
+
 })
